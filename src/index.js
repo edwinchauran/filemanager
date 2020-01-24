5c78d3cfaa4ba5452a9bf8b47f88f75f4d45d54f
edwinvisuales <thedwinvisual@gmail.com>
2020-01-24 13:32:24 -0400

    port 8080 to 80

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const multer = require('multer');
 const app = express();
 require('./database');
 // Settings
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 80);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('.hbs', exphbs({
     defaultLayaout: 'main',
